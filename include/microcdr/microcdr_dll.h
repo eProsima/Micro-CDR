4b40d88d9a38763b93d53f0f2093ee18629158bb
julianbermudez <julianbermudez@eprosima.com>
2018-04-27 05:42:16 +0200

    Refs #2838. Checked installation on Windows.

--- include/microcdr/microcdr_dll.h
+++ include/microcdr/microcdr_dll.h
@@ -18,52 +18,18 @@
 
 #include <microcdr/config.h>
 
-// normalize macros
-#if !defined(MICROCDR_DYN_LINK) && !defined(MICROCDR_STATIC_LINK) \
-    && !defined(EPROSIMA_ALL_DYN_LINK) && !defined(EPROSIMA_ALL_STATIC_LINK)
-#define MICROCDR_STATIC_LINK
-#endif
-
-#if defined(EPROSIMA_ALL_DYN_LINK) && !defined(MICROCDR_DYN_LINK)
-#define MICROCDR_DYN_LINK
-#endif
-
-#if defined(MICROCDR_DYN_LINK) && defined(MICROCDR_STATIC_LINK)
-#error Must not define both MICROCDR_DYN_LINK and MICROCDR_STATIC_LINK
-#endif
-
-#if defined(EPROSIMA_ALL_NO_LIB) && !defined(MICROCDR_NO_LIB)
-#define MICROCDR_NO_LIB
-#endif
-
-// enable dynamic linking
 #if defined(_WIN32)
-#if defined(EPROSIMA_ALL_DYN_LINK) || defined(MICROCDR_DYN_LINK)
-#if defined(MICROCDR_SOURCE)
+#if defined(BUILDING_SHARED_LIB)
+#if defined(microcdr_EXPORTS)
 #define microcdr_DllAPI __declspec( dllexport )
 #else
 #define microcdr_DllAPI __declspec( dllimport )
-#endif // MICROCDR_SOURCE
+#endif // microcdr_EXPORTS
 #else
 #define microcdr_DllAPI
-#endif
+#endif // BUILDING_SHARED_LIBS
 #else
 #define microcdr_DllAPI
 #endif // _WIN32
-
-// Auto linking.
-
-#if !defined(MICROCDR_SOURCE) && !defined(EPROSIMA_ALL_NO_LIB) \
-    && !defined(MICROCDR_NO_LIB)
-
-// Set properties.
-#define EPROSIMA_LIB_NAME microcdr
-
-#if defined(EPROSIMA_ALL_DYN_LINK) || defined(MICROCDR_DYN_LINK)
-#define EPROSIMA_DYN_LINK
-#endif
-
-#include "eProsima_auto_link.h"
-#endif // auto-linking disabled
 
 #endif // _MICROCDR_MICROCDR_DLL_H_
